4dc79b9b287a468b00f0d30eb98606c8c279183e
Melonanas <Melonanas1@gmail.com>
2024-07-24 17:09:24 +0200

    Tried changing EnemyFinder functionality to distance calculation
    currently throws "object reference not set to an instance of an object" in EnemyFinder.GetClosestEnemy()

--- Assets/Code/Scripts/Combat/EnemyFinder.cs
+++ Assets/Code/Scripts/Combat/EnemyFinder.cs
@@ -18,12 +18,23 @@ namespace Combat
     public class EnemyFinder : MonoBehaviour
     {
         [SerializeField] private List<GameObject> enemies = new List<GameObject>();
+        private Transform enemiesParent;
+
+        float closestDistanceSqr;
 
         private SphereCollider detectionArea;
 
         private void Awake()
         {
             detectionArea = GetComponent<SphereCollider>();
+            if (enemiesParent == null)
+            {
+                enemiesParent = GameObject.Find("EnemySpawner").transform;
+                if (enemiesParent == null)
+                {
+                    Debug.LogError("EnemiesParent not found in the scene!");
+                }
+            }
         }
 
         internal void Initialize(CharacterStats stats)
@@ -35,46 +46,24 @@ namespace Combat
         public GameObject GetClosestEnemy()
         {
             GameObject closestEnemy = null;
-            foreach (GameObject enemy in enemies)
-            {
-                if (closestEnemy == null) closestEnemy = enemy;
-                else
-                {
-                    float currentClosestDistance = Vector3.SqrMagnitude(closestEnemy.transform.position - transform.position);
-                    float newDistance = Vector3.SqrMagnitude(enemy.transform.position - transform.position);
+            closestDistanceSqr = Mathf.Infinity;
 
-                    if (newDistance < currentClosestDistance) closestEnemy = enemy;
+            foreach (Transform enemyTransform in enemiesParent)
+            {
+                float distanceSqr = (enemyTransform.position - transform.position).sqrMagnitude;
+                if (distanceSqr < closestDistanceSqr)
+                {
+                    closestEnemy = enemyTransform.gameObject;
+                    closestDistanceSqr = distanceSqr;
                 }
             }
+
             return closestEnemy;
         }
 
         public bool EnemyInRange()
         {
-            return enemies.Count != 0;
-        }
-
-        private void OnTriggerEnter(Collider other)
-        {
-            if (other.gameObject.CompareTag("Enemy"))
-            {
-                other.gameObject.GetComponent<Health>().onDeath += RemoveEnemyFromList;
-                enemies.Add(other.gameObject);
-            }
-        }
-
-        private void OnTriggerExit(Collider other)
-        {
-            if (other.gameObject.CompareTag("Enemy"))
-            {
-                other.gameObject.GetComponent<Health>().onDeath -= RemoveEnemyFromList;
-                enemies.Remove(other.gameObject);
-            }
-        }
-
-        private void RemoveEnemyFromList(GameObject enemy)
-        {
-            enemies.Remove(enemy);
+            return closestDistanceSqr < Math.Pow(detectionArea.radius,2);
         }
 
         private void UpdateAttackRange(Stat stat, float newValue)
