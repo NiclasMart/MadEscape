f1a2513b9b54e71f53b3149440e166615a719435
Niclas Mart <salcintram07@web.de>
2025-04-14 21:29:27 +0200

    save scenes before switching

--- Assets/Editor/LoadScenes.cs
+++ Assets/Editor/LoadScenes.cs
@@ -9,6 +9,7 @@ public class LoadScenes
     [MenuItem("Game Tools/Load Scene/Main")]
     public static void LoadMain()
     {
+        CheckForChanges();
         EditorSceneManager.OpenScene("Assets/Content/Scenes/Main.unity");
         EditorSceneManager.OpenScene("Assets/Content/Scenes/TestEnemyAI.unity", OpenSceneMode.Additive);
     }
@@ -16,6 +17,35 @@ public class LoadScenes
     [MenuItem("Game Tools/Load Scene/Weapon Test")]
     public static void LoadWeaponTest()
     {
+        CheckForChanges();
         EditorSceneManager.OpenScene("Assets/Content/Scenes/Weapon_Test.unity");
     }
+
+    private static void CheckForChanges()
+    {
+        bool unsafedChanges = false;
+        for (int i = 0; i < EditorSceneManager.sceneCount; i++)
+        {
+            if (EditorSceneManager.GetSceneAt(i).isDirty)
+            {
+                unsafedChanges = true;
+            }
+        }
+
+        if (!unsafedChanges) return;
+
+        bool shouldSave = EditorUtility.DisplayDialog(
+                    "Unsaved Changes",
+                    "You have unsaved changes. Do you want to save them before switching scenes?",
+                    "Save", // Button 1
+                    "Don't Save" // Button 2
+        );
+
+        if (shouldSave)
+        {
+            EditorSceneManager.SaveScene(EditorSceneManager.GetActiveScene());
+        }
+
+    }
+
 }
